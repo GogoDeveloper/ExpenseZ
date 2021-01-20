06541089e1e8c63b1211cd545ff56a260fc26637
GogoDeveloper <gogomagnusdev@gmail.com>
2021-01-20 20:37:50 +0100

    finished splash screen with full animation functionalities

--- app/src/main/java/ch/zli/expensez/SplashScreenActivity.java
+++ app/src/main/java/ch/zli/expensez/SplashScreenActivity.java
@@ -2,17 +2,68 @@ package ch.zli.expensez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    Animation logoAnimation, sloganAnimation;
+    ImageView logo;
+    TextView slogan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        logoAnimation = AnimationUtils.loadAnimation(this, R.anim.logo_animation);
+        sloganAnimation = AnimationUtils.loadAnimation(this, R.anim.slogan_animation);
+
+        logo = findViewById(R.id.imgLogo);
+        slogan = findViewById(R.id.txtSlogan);
+
+
+        logo.setAnimation(logoAnimation);
+        slogan.setAnimation(sloganAnimation);
+
+
+
+         new Handler().postDelayed(() -> {
+             Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+             startActivity(mainIntent);
+             finish();
+         }, 5000);
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
