33256919d8d386cd0006352c25cb34499bb2ba96
GogoDeveloper <gogomagnusdev@gmail.com>
2021-01-20 23:19:09 +0100

    added editincome activity

---
+++ app/src/main/java/ch/zli/expensez/EditIncomeActivity.java
@@ -0,0 +1,14 @@
+package ch.zli.expensez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditIncomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_income);
+    }
+}
